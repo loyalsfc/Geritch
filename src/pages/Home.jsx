40f560c10e30f270ed6a46ef10b2b3f42bb3666d
Olumide Bambe <loyalsfc@gmail.com>
2024-06-01 14:24:57 +0100

    fix broken link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,7 +68,7 @@ function Home() {
                         <Icon />
                         <h1 className='font-comorant font-bold leading-[120%] text-5xl md:text-7xl lg:text-[90px] text-primary tracking-[0.04em]'>The key to Fine dining</h1>
                         <p className='leading-[175%] tracking-[0.04em] mb-2'>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-                        <Link to="/page"><button className='btn-pry'>Explore Menu</button></Link>
+                        <Link to="/meals"><button className='btn-pry'>Explore Menu</button></Link>
                     </motion.article>
                     <ImageFrame image={hero} />
                     <div onClick={()=>
